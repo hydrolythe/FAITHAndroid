511c0d583664de668d512f77d18fdbde656c5882
Harm De Weirdt <harm.deweirdt@mailfence.com>
2019-01-24 18:08:31 +0100

    fix: Fix EventTest to use correct LocalDateTime
    The test wasn't updated to reflect recent changes to Event.

---
+++ domain/src/test/java/be/hogent/faith/domain/EventTest.kt
@@ -0,0 +1,50 @@
+package be.hogent.faith.domain
+
+import be.hogent.faith.domain.models.Detail
+import be.hogent.faith.domain.models.Event
+import io.mockk.mockk
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.threeten.bp.LocalDateTime
+import org.threeten.bp.Month
+
+class EventTest {
+
+    private lateinit var event: Event
+
+    @Before
+    fun setUp() {
+        event = Event(mockk(), "testDescription")
+    }
+
+    @Test
+    fun `Event constructor correctly sets attributes`() {
+        val date = LocalDateTime.of(1991, Month.OCTOBER, 28, 8, 33)
+        val description = "testDescription"
+
+        val newEvent = Event(date, description)
+
+        assertEquals(description, newEvent.description)
+        assertEquals(date, newEvent.dateTime)
+    }
+
+    @Test
+    fun `Event addDetail correctly adds the detail`() {
+        val detail = mockk<Detail>()
+
+        event.addDetail(detail)
+
+        assertEquals(1, event.details.size)
+        assertEquals(detail, event.details.first())
+    }
+
+    @Test
+    fun `Event getDetails returns all details`() {
+        for (i in 1..10) {
+            event.addDetail(mockk())
+        }
+
+        assertEquals(10, event.details.size)
+    }
+}
