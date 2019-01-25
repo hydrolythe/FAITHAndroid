9ea02b9ddfbdc3c2d38e0306caaeac532ffc1c60
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-01-25 10:10:37 +0100

    tests: Adds starts of UC tests

--- service/src/test/java/be/hogent/faith/service/usecases/GetEventsUseCaseTest.kt
+++ service/src/test/java/be/hogent/faith/service/usecases/GetEventsUseCaseTest.kt
@@ -1,10 +1,36 @@
 package be.hogent.faith.service.usecases
 
+import be.hogent.faith.domain.repository.EventRepository
+import io.mockk.mockk
+import io.mockk.verify
+import io.reactivex.Scheduler
+import org.junit.Before
 import org.junit.Test
+import java.util.concurrent.Executor
 
 class GetEventsUseCaseTest {
+    private lateinit var getEventsUC: GetEventsUseCase
+    private lateinit var executor: Executor
+    private lateinit var scheduler: Scheduler
+    private lateinit var repository: EventRepository
+
+
+    @Before
+    fun setUp() {
+        executor = mockk()
+        scheduler = mockk()
+        repository = mockk()
+        getEventsUC = GetEventsUseCase(repository, scheduler)
+    }
+
     @Test
-    fun getEventsUseCase_eventsPresent_returnsThem(){
+    fun getEventsUC_execute_callsRepo() {
+        getEventsUC.execute(mockk())
+        verify { repository.getAll() }
+    }
+
+    @Test
+    fun getEventsUseCase_eventsPresent_returnsThem() {
 
     }
 
@@ -12,4 +38,6 @@ class GetEventsUseCaseTest {
     fun getEventsUseCase_noEventsPresent_returnsNothing() {
 
     }
+
+
 }
