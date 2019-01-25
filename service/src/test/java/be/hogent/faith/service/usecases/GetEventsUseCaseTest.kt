a4964e284cb790678633d679f08153e1b9bc8559
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-01-25 11:21:44 +0100

    test: Adds test for GetEventsUseCase

--- service/src/test/java/be/hogent/faith/service/usecases/GetEventsUseCaseTest.kt
+++ service/src/test/java/be/hogent/faith/service/usecases/GetEventsUseCaseTest.kt
@@ -1,8 +1,11 @@
 package be.hogent.faith.service.usecases
 
+import be.hogent.faith.domain.models.Event
 import be.hogent.faith.domain.repository.EventRepository
+import io.mockk.every
 import io.mockk.mockk
 import io.mockk.verify
+import io.reactivex.Flowable
 import io.reactivex.Scheduler
 import org.junit.Before
 import org.junit.Test
@@ -19,24 +22,35 @@ class GetEventsUseCaseTest {
     fun setUp() {
         executor = mockk()
         scheduler = mockk()
-        repository = mockk()
+        repository = mockk(relaxed = true)
         getEventsUC = GetEventsUseCase(repository, scheduler)
     }
 
     @Test
     fun getEventsUC_execute_callsRepo() {
-        getEventsUC.execute(mockk())
+        getEventsUC.buildUseCaseObservable(mockk())
         verify { repository.getAll() }
     }
 
     @Test
     fun getEventsUseCase_eventsPresent_returnsThem() {
-
+        //Simulate two events on the stream
+        every { repository.getAll() } returns Flowable.just(
+            listOf(createEvent(), createEvent()),
+            listOf(createEvent(), createEvent()))
+        val result = getEventsUC.buildUseCaseObservable(mockk())
+        result.test().assertValueCount(2)
     }
 
     @Test
     fun getEventsUseCase_noEventsPresent_returnsNothing() {
+        every { repository.getAll() } returns Flowable.empty()
+        val result = getEventsUC.buildUseCaseObservable(mockk())
+        result.test().assertNoValues()
+    }
 
+    private fun createEvent(): Event {
+        return Event(mockk(), "description", mockk())
     }
 
 
