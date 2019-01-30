ed2d0d9bd163d60820d8da599defbff3ee269230
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-01-30 15:01:36 +0100

    test: Adds test that clicks all drawing buttons

--- app/src/androidTest/java/be/hogent/faith/faith/drawEmotionAvatar/DrawEmotionAvatarFragmentTest.kt
+++ app/src/androidTest/java/be/hogent/faith/faith/drawEmotionAvatar/DrawEmotionAvatarFragmentTest.kt
@@ -1,7 +1,11 @@
 package be.hogent.faith.faith.drawEmotionAvatar
 
 import androidx.fragment.app.testing.launchFragmentInContainer
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.runner.AndroidJUnit4
+import be.hogent.faith.R
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -10,5 +14,22 @@ class DrawEmotionAvatarFragmentTest {
     @Test
     fun drawEmotionFragment_launches() {
         launchFragmentInContainer<DrawEmotionAvatarFragment>()
+    }
+
+    @Test
+    fun drawEmotionFragment_canClickAllButtons() {
+        launchFragmentInContainer<DrawEmotionAvatarFragment>()
+
+        onView(withId(R.id.btn_draw_selectEraser)).perform(click())
+
+        onView(withId(R.id.btn_draw_setColorBlack)).perform(click())
+        onView(withId(R.id.btn_draw_setColorBlue)).perform(click())
+        onView(withId(R.id.btn_draw_setColorGreen)).perform(click())
+        onView(withId(R.id.btn_draw_setColorRed)).perform(click())
+        onView(withId(R.id.btn_draw_setColorYellow)).perform(click())
+
+        onView(withId(R.id.btn_draw_setThinLineWidth)).perform(click())
+        onView(withId(R.id.btn_draw_setMediumLineWidth)).perform(click())
+        onView(withId(R.id.btn_draw_setThickLineWidth)).perform(click())
     }
 }
