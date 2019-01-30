05966a85802d441b13f1da594677e914d9f38b89
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-01-30 16:52:16 +0100

    feat: Adds simple drawing and background
    Uses Android Draw library.
    Still TODO: move and crop the background image so it stays in the middle.
    Bug: when the screen isn't large enoughs some buttons go out of bounds.

--- app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
+++ app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
@@ -3,7 +3,7 @@ package be.hogent.faith.faith.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import be.hogent.faith.R
-import be.hogent.faith.faith.createUser.CreateEventFragment
+import be.hogent.faith.faith.drawEmotionAvatar.DrawEmotionAvatarFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         supportFragmentManager.beginTransaction()
-            .add(R.id.fragment_container, CreateEventFragment())
+            .add(R.id.fragment_container, DrawEmotionAvatarFragment())
             .commit()
     }
 }
