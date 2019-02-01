e8b58c4246d21ce980dd9434cbebc560ab6015a8
Harm De Weirdt <harm.deweirdt@mailfence.com>
2019-02-01 13:57:57 +0100

    feat: Adds basic implementation of moving avatar
    After considering some options, I chose to use a Framelayout on which
    the avatar, the background and a constraintlayout are placed. The
    constraintlayout defines clickable areas by using guidelines. As the
    number of clickable areas should remain rather small, this is (imo)
    still OK.
    
    The alternatives, working with pre-defined areas defined by coordinates
    doesn't seem an option because this doesn't work on all screen
    resolutions.

--- app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
+++ app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
@@ -3,14 +3,15 @@ package be.hogent.faith.faith.ui
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import be.hogent.faith.R
-import be.hogent.faith.faith.drawEmotionAvatar.DrawEmotionAvatarFragment
+import be.hogent.faith.faith.mainScreen.MainScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val fragment = DrawEmotionAvatarFragment.newInstance(R.drawable.outline)
+//        val fragment = DrawEmotionAvatarFragment.newInstance(R.drawable.outline)
+        val fragment = MainScreenFragment()
         supportFragmentManager.beginTransaction()
             .add(R.id.fragment_container, fragment)
             .commit()
