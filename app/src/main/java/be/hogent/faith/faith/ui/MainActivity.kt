adf62466f0dcb4ecd8d81982b76516199ef32970
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-13 11:03:30 +0100

    refactor: remove unused import

--- app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
+++ app/src/main/java/be/hogent/faith/faith/ui/MainActivity.kt
@@ -4,7 +4,6 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import be.hogent.faith.R
 import be.hogent.faith.faith.drawEmotionAvatar.DrawEmotionAvatarFragment
-import be.hogent.faith.faith.mainScreen.MainScreenFragment
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
