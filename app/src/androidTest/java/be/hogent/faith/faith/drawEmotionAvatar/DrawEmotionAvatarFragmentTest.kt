594c816ecc07a571ed738579ac4aea3130ed7fbe
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-13 10:52:53 +0100

    refactor: Replace deprecated AndroidJunit runner

--- app/src/androidTest/java/be/hogent/faith/faith/drawEmotionAvatar/DrawEmotionAvatarFragmentTest.kt
+++ app/src/androidTest/java/be/hogent/faith/faith/drawEmotionAvatar/DrawEmotionAvatarFragmentTest.kt
@@ -4,7 +4,7 @@ import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import be.hogent.faith.R
 import org.junit.Test
 import org.junit.runner.RunWith
--- app/src/androidTest/java/be/hogent/faith/faith/mainScreen/MainScreenFragmentTest.kt
+++ app/src/androidTest/java/be/hogent/faith/faith/mainScreen/MainScreenFragmentTest.kt
@@ -1,7 +1,7 @@
 package be.hogent.faith.faith.mainScreen
 
 import androidx.fragment.app.testing.launchFragmentInContainer
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
