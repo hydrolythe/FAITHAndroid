19d4b09f04f37242f5776fef95e25a8e2c848672
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-14 14:43:35 +0100

    feat: Add saving of drawn images
    Adds a storage module that's responsible for saving media (audio,
    video, images,...) to the phone's internal and external storage.
    
    Bugs present: drawings are only saved partially. Something with the
    paintedBackground and the non-resized version being out-of-sync when
    drawing to the bitmap.
    FAITH-8

---
+++ storage/src/main/java/be/hogent/faith/storage/StorageRepository.kt
@@ -0,0 +1,41 @@
+package be.hogent.faith.storage
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.util.Log
+import be.hogent.faith.domain.models.Event
+import io.reactivex.Observable
+import io.reactivex.Single
+import org.threeten.bp.format.DateTimeFormatter
+import java.io.File
+import java.io.IOException
+
+/**
+ * Repository providing access to both the internal and external storage.
+ * It decides which one will be used based on the user's settings.
+ *
+ * TODO: Currently only supports internal storage.
+ */
+class StorageRepository(private val context: Context) {
+
+    fun storeBitmap(bitmap: Bitmap, event: Event): Single<File> {
+        return try {
+            val storedFile =
+                File(createEventImageFolder(event), event.dateTime.format(DateTimeFormatter.ofPattern("yMdHms")))
+            Log.i("StorageRepo", "Filename will be ${storedFile.absolutePath}")
+            storedFile.outputStream().use {
+                bitmap.compress(Bitmap.CompressFormat.PNG, 100, it)
+        }
+            Single.fromObservable(Observable.just(storedFile))
+        } catch (e: IOException) {
+            Single.error(e)
+        }
+    }
+
+    private fun createEventImageFolder(event: Event): File {
+        val imageDirectory = File(context.filesDir, "events/${event.uuid}/images")
+        imageDirectory.mkdirs()
+        return imageDirectory
+    }
+
+}
