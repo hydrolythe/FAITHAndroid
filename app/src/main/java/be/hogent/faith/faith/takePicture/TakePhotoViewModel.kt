1e6d42b1e9818a034f9f81407ec5e773074ff226
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-25 15:15:36 +0100

    feat: Forward UI events to ViewModel

--- app/src/main/java/be/hogent/faith/faith/takePicture/TakePhotoViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/takePicture/TakePhotoViewModel.kt
@@ -1,5 +1,23 @@
 package be.hogent.faith.faith.takePicture
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import be.hogent.faith.faith.util.SingleLiveEvent
 
-class TakePhotoViewModel : ViewModel()
+class TakePhotoViewModel : ViewModel() {
+    private val _takePhotoButtonClicked = SingleLiveEvent<Unit>()
+    val takePhotoButtonClicked: LiveData<Unit>
+        get() = _takePhotoButtonClicked
+
+    private val _emotionAvatarButtonClicked = SingleLiveEvent<Unit>()
+    val emotionAvatarButtonClicked: LiveData<Unit>
+        get() = _emotionAvatarButtonClicked
+
+    fun onTakePhotoButtonClicked() {
+        _takePhotoButtonClicked.call()
+    }
+
+    fun onGoToEmotionAvatarButtonClicked() {
+        _emotionAvatarButtonClicked.call()
+    }
+}
