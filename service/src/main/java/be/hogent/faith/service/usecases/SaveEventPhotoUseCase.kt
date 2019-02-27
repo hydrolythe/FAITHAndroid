58a13afeee0da23a8f6b451f4b9123b9dc17235a
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-27 14:20:38 +0100

    fix: use DateTime instead of Date for saveFile
    Date doesn't have an hour/minute/second division which of course makes
    it impossible to format it as such.

--- service/src/main/java/be/hogent/faith/service/usecases/SaveEventPhotoUseCase.kt
+++ service/src/main/java/be/hogent/faith/service/usecases/SaveEventPhotoUseCase.kt
@@ -9,7 +9,7 @@ import be.hogent.faith.storage.StorageRepository
 import io.reactivex.Completable
 import io.reactivex.Scheduler
 import io.reactivex.schedulers.Schedulers
-import org.threeten.bp.LocalDate
+import org.threeten.bp.LocalDateTime
 import org.threeten.bp.format.DateTimeFormatter
 
 class SaveEventPhotoUseCase(
@@ -37,7 +37,7 @@ class SaveEventPhotoUseCase(
      * day_month_year_hour_minute_second_millis
      */
     private fun createSaveFileName(): String {
-        return LocalDate.now().format(DateTimeFormatter.ofPattern("d_M_y_H_m_s_A"))
+        return LocalDateTime.now().format(DateTimeFormatter.ofPattern("d_M_y_Ka_m_s_A"))
     }
 
     data class Params(val bitmap: Bitmap, val event: Event)
