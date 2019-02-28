fd9accec5a38df43631058232e9a3e857bad3abe
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-02-28 08:42:12 +0100

    feat: add listeners to VM and layout

--- app/src/main/java/be/hogent/faith/faith/recordAudio/RecordAudioViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/recordAudio/RecordAudioViewModel.kt
@@ -1,5 +1,17 @@
 package be.hogent.faith.faith.recordAudio
 
+import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import be.hogent.faith.faith.util.SingleLiveEvent
 
-class RecordAudioViewModel : ViewModel()
+class RecordAudioViewModel : ViewModel() {
+
+    private val _recordButtonClicked = SingleLiveEvent<Unit>()
+    val recordButtonClicked: LiveData<Unit>
+        get() = _recordButtonClicked
+
+    fun onRecordButtonClicked() {
+        _recordButtonClicked.call()
+    }
+
+}
