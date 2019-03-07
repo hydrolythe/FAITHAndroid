bf49fa43d7ec0744599646a70e65391fa895cc8f
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-03-07 09:29:25 +0200

    fix: remove unused params

--- database/src/main/java/be/hogent/faith/database/di/DatabaseModule.kt
+++ database/src/main/java/be/hogent/faith/database/di/DatabaseModule.kt
@@ -14,7 +14,7 @@ val databaseModule = module {
     single { EntityDatabase.getDatabase(androidContext()) }
     single { constructEventDao(get()) }
     single { constructDetailDao(get()) }
-    // Just specifying the EventRepositoryImpl is not enough.
+    // Just specifying the aventRepositoryImpl is not enough.
     // In other modules some elements require an EventRepository as constructor parameter.
     // Koin doesn't automatically see the Impl as an implementation of the interface,
     // so we have to explicitly mention it.
