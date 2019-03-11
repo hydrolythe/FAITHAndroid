b76a6986d2975e72130399896cd6ede8ab990993
Harm De Weirdt <harm.deweirdt@mailfence.com>
2019-03-11 15:59:22 +0100

    Revert "Gebeurtenis aanmaken/avatar inkleuren fixes"

--- app/src/main/java/be/hogent/faith/faith/util/Extensions.kt
+++ app/src/main/java/be/hogent/faith/faith/util/Extensions.kt
@@ -1,12 +1,7 @@
 package be.hogent.faith.faith.util
 
-import android.content.Context
-import android.widget.Toast
-
 val Any.TAG: String
     get() {
         val tag = javaClass.simpleName
         return if (tag.length <= 23) tag else tag.substring(0, 23)
     }
-fun Context.toast(message: CharSequence) =
-    Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
