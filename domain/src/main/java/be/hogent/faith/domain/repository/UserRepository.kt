875244649dd18eb5837d73571fdd8de1ecd3ec30
Karine Samyn <Karine.Samyn@hogent.be>
2019-03-12 17:48:45 +0100

    <feat> verwijderen van de repository interface
    de interface is te algemeen en niet bruikbaar voor user, events

--- domain/src/main/java/be/hogent/faith/domain/repository/UserRepository.kt
+++ domain/src/main/java/be/hogent/faith/domain/repository/UserRepository.kt
@@ -1,5 +1,17 @@
 package be.hogent.faith.domain.repository
 
 import be.hogent.faith.domain.models.User
+import io.reactivex.Completable
+import io.reactivex.Flowable
+import java.util.UUID
 
-interface UserRepository : Repository<User>
+interface UserRepository {
+
+    fun delete(item: User): Completable
+
+    fun insert(item: User): Completable
+
+    fun get(uuid: UUID): Flowable<User>
+
+    fun getAll(): Flowable<List<User>>
+}
