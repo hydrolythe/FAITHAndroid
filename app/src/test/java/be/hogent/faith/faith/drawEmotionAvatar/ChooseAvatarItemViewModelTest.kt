65ea42a61e6e3579075be3e1e9ce75cdebf037dc
eothein <jens.buysse@gmail.com>
2019-03-12 16:09:13 +0100

    [test] Add test for the AvatarItemViewModel

---
+++ app/src/test/java/be/hogent/faith/faith/drawEmotionAvatar/ChooseAvatarItemViewModelTest.kt
@@ -0,0 +1,39 @@
+package be.hogent.faith.faith.drawEmotionAvatar
+
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import be.hogent.faith.faith.TestUtils.getValue
+import be.hogent.faith.faith.chooseAvatar.fragments.AvatarItemViewModel
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+
+class ChooseAvatarItemViewModelTest {
+
+    val naam: String = "Boaty McBoatface"
+    val selection : Int = 1
+
+    private lateinit var viewModel: AvatarItemViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = AvatarItemViewModel()
+    }
+
+    @get:Rule
+    val testRule = InstantTaskExecutorRule()
+
+
+    @Test
+    fun avatarItemViewModel_setUserName() {
+        viewModel.setUserName(naam)
+        Assert.assertEquals(naam,getValue(viewModel.userName))
+    }
+
+    @Test
+    fun avatarItemViewModel_setSelectedItem(){
+        viewModel.setSelectedItem(selection.toLong())
+        Assert.assertEquals(selection, getValue(viewModel.selectedItem))
+    }
+
+}
