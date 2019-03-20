9c13696e04b7029acc07a978e0b26b7b32113118
Karine Samyn <Karine.Samyn@hogent.be>
2019-03-20 16:02:06 +0100

    fix:make AvatarViewModelTest work

--- app/src/test/java/be/hogent/faith/faith/chooseAvatar/ChooseAvatarViewModelTest.kt
+++ app/src/test/java/be/hogent/faith/faith/chooseAvatar/ChooseAvatarViewModelTest.kt
@@ -1,8 +1,10 @@
 package be.hogent.faith.faith.chooseAvatar
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import be.hogent.faith.faith.App
 import be.hogent.faith.faith.TestUtils.getValue
 import be.hogent.faith.faith.chooseAvatar.fragments.AvatarViewModel
+import io.mockk.mockk
 import org.junit.Assert
 import org.junit.Before
 import org.junit.Rule
@@ -12,12 +14,13 @@ class ChooseAvatarViewModelTest {
 
     private val naam: String = "Boaty McBoatface"
     private val selection: Int = 1
+    private val application = mockk<App>(relaxed = true)
 
     private lateinit var viewModel: AvatarViewModel
 
     @Before
     fun setUp() {
-        viewModel = AvatarViewModel()
+        viewModel = AvatarViewModel(application)
     }
 
     @get:Rule
