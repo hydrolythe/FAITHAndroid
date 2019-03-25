ccba3dc35cc2f405f8c197adc714645b47b950aa
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-03-25 15:08:55 +0100

    feat: add viewmodel with databinding

--- app/src/main/java/be/hogent/faith/faith/enterEventDetails/EventDetailsViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/enterEventDetails/EventDetailsViewModel.kt
@@ -1,14 +1,23 @@
 package be.hogent.faith.faith.enterEventDetails
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import be.hogent.faith.domain.models.Event
 import be.hogent.faith.domain.models.User
 import be.hogent.faith.faith.util.SingleLiveEvent
+import be.hogent.faith.faith.util.TAG
+import be.hogent.faith.service.usecases.SaveEventUseCase
+import io.reactivex.disposables.CompositeDisposable
+import org.threeten.bp.LocalDateTime
 import java.util.UUID
 
-class EventDetailsViewModel(val user: LiveData<User>, eventUuid: UUID? = null) : ViewModel() {
+class EventDetailsViewModel(
+    val user: LiveData<User>,
+    eventUuid: UUID? = null,
+    private val saveEventUseCase: SaveEventUseCase
+) : ViewModel() {
 
     /**
      * The event that will be discussed and explained using audio, video, drawings,...
@@ -16,10 +25,25 @@ class EventDetailsViewModel(val user: LiveData<User>, eventUuid: UUID? = null) :
     val event = MutableLiveData<Event>()
 
     /**
-     * The title of the event (optional when on the main entry screen.
+     * The title of the event.
+     * It's optional on the main event screen, but has to be filled in when saving the event.
      * We can't observe it here, so the Fragment observes it and changes the [event] when required.
      */
     val eventTitle = MutableLiveData<String>()
+
+    /**
+     * The title of the event (optional when on the main entry screen.
+     * We can't observe it here, so the Fragment observes it and changes the [event] when required.
+     */
+    val eventDate = MutableLiveData<LocalDateTime>()
+
+    /**
+     * The notes that are added when saving the event
+     * We can't observe it here, so the Fragment observes it and changes the [event] when required.
+     */
+    val eventNotes = MutableLiveData<String>()
+
+    private val disposables = CompositeDisposable()
 
     init {
         if (eventUuid != null) {
@@ -54,16 +78,24 @@ class EventDetailsViewModel(val user: LiveData<User>, eventUuid: UUID? = null) :
     val emotionAvatarClicked: LiveData<Unit>
         get() = _emotionAvatarClicked
 
+    private val _cancelButtonClicked = SingleLiveEvent<Unit>()
+    val cancelButtonClicked: LiveData<Unit>
+        get() = _cancelButtonClicked
+
+    private val _dateButtonClicked = SingleLiveEvent<Unit>()
+    val dateButtonClicked: LiveData<Unit>
+        get() = _dateButtonClicked
+
     init {
         event.value = Event()
     }
 
     /**
-     * Helper method to be called when changing one of the properties of the [event].
+     * Helper method to be called when changing one of the properties of the [Event].
      * This is needed because just changing properties doesn't call all subscribers, only changing the actual value does.
      */
     fun updateEvent() {
-        event.value = event.value
+        event.postValue(event.value)
     }
 
     fun onCameraButtonClicked() {
@@ -86,7 +118,30 @@ class EventDetailsViewModel(val user: LiveData<User>, eventUuid: UUID? = null) :
         _sendButtonClicked.call()
     }
 
+    fun onCancelButtonClicked() {
+        _cancelButtonClicked.call()
+    }
+
+    fun onDateButtonClicked() {
+        _dateButtonClicked.call()
+    }
+
+    fun onSaveButtonClicked() {
+        val params = SaveEventUseCase.Params(event.value!!, user.value!!)
+        val disposable = saveEventUseCase.execute(params).subscribe({
+            Log.i(TAG, "Event saved")
+        }, {
+            Log.i(TAG, "Event failed to save")
+        })
+        disposables.add(disposable)
+    }
+
     fun onEmotionAvatarClicked() {
         _emotionAvatarClicked.call()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        disposables.clear()
     }
 }
