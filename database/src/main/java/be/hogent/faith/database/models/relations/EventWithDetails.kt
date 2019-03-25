907ce32baf4be9c989d562aba037b5c7e48ac41f
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-03-25 10:55:44 +0100

    docs: add docs for recent DetailType changes
    FAITH-32

--- database/src/main/java/be/hogent/faith/database/models/relations/EventWithDetails.kt
+++ database/src/main/java/be/hogent/faith/database/models/relations/EventWithDetails.kt
@@ -9,7 +9,11 @@ import be.hogent.faith.database.models.detail.PictureDetailEntity
 import be.hogent.faith.database.models.detail.TextDetailEntity
 
 /**
- * Relation class for the [EventEntity] and it's relation with [DetailEntity]s
+ * Relation class for the [EventEntity] and it's relation with [DetailEntity]s.
+ * Holds a separate list for each subtype of [DetailEntity]. The reasoning behind this can be found in the [DetailDao].
+ * When requesting the detailEntities it combines these three lists into one.
+ * When you need to set the detailEntities manually (when testing for example), use the [addDetailEntities] method.
+ * This will classify the details in your provided list to the appropriate list.
  */
 class EventWithDetails {
     @Embedded
@@ -24,7 +28,10 @@ class EventWithDetails {
     @Relation(parentColumn = "uuid", entityColumn = "eventUuid", entity = PictureDetailEntity::class)
     var pictureDetailEntities: MutableList<PictureDetailEntity> = mutableListOf()
 
-    // Keep as a var so it can be set in tests.
+    /**
+     * The combined list of all [DetailEntity]s.
+     * If you need to set this, use [addDetailEntities].
+     */
     val detailEntities: List<DetailEntity>
         get() {
             return mutableListOf<DetailEntity>().apply {
@@ -34,6 +41,10 @@ class EventWithDetails {
             }
         }
 
+    /**
+     * Adds a list of [DetailEntity]s.
+     * Will place every given entity in the appropriate list.
+     */
     fun addDetailEntities(entities: List<DetailEntity>) {
         entities.forEach {
             when (it) {
