2b5d3dbba8bdbf3745aad05a1bc6efdc1ae19f1e
Karine Samyn <Karine.Samyn@hogent.be>
2019-04-02 12:27:18 +0200

    fi< : removed listener

--- app/src/main/java/be/hogent/faith/faith/ui/BackToCityListener.kt
+++
@@ -1,7 +0,0 @@
-package be.hogent.faith.faith.ui
-
-interface BackToCityListener {
-    fun needConfirmation(): Boolean
-    fun getConfirmationTitle(): String
-    fun getConfirmationMessage():String
-}
