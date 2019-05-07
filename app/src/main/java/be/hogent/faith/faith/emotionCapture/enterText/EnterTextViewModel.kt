aa0e436be0f61424371b5126cdaf0e38712c7509
Karine Samyn <Karine.Samyn@hogent.be>
2019-05-07 16:22:56 +0200

    feat:added enterTextFragment and ViewModel

--- app/src/main/java/be/hogent/faith/faith/emotionCapture/enterText/EnterTextViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/emotionCapture/enterText/EnterTextViewModel.kt
@@ -1,20 +1,31 @@
 package be.hogent.faith.faith.emotionCapture.enterText
 
+import android.graphics.Color
 import android.util.Log
 import androidx.annotation.ColorInt
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import be.hogent.faith.domain.models.Event
+import be.hogent.faith.faith.emotionCapture.drawing.DrawViewModel
 import be.hogent.faith.faith.util.SingleLiveEvent
+import be.hogent.faith.util.TAG
 
 class EnterTextViewModel(
     private val event: Event
 ):ViewModel() {
 
+    private val _text = MutableLiveData<String>()
+    val text:LiveData<String>
+    get()=_text
+
     private val _selectedTextColor = MutableLiveData<@ColorInt Int>()
     val selectedTextColor: LiveData<Int>
         get() = _selectedTextColor
+
+    private val _selectedFontSize = MutableLiveData<@ColorInt Int>()
+    val selectedFontSize: LiveData<Int>
+        get() = _selectedFontSize
 
     private val _boldClicked = SingleLiveEvent<Unit>()
     val boldClicked: LiveData<Unit>
@@ -27,6 +38,11 @@ class EnterTextViewModel(
     private val _underlineClicked = SingleLiveEvent<Unit>()
     val underlineClicked: LiveData<Unit>
         get() = _underlineClicked
+
+    init {
+        _selectedTextColor.value = Color.BLACK
+        _selectedFontSize.value = 5
+    }
 
     fun onBoldClicked(){
         _boldClicked.call()
@@ -48,4 +64,20 @@ class EnterTextViewModel(
         _selectedTextColor.value = color
     }
 
+    //de editor voorziet in waarden van 1..7
+    fun pickFontSize(fontSize:FontSize){
+        _selectedFontSize.value = fontSize.size;
+    }
+
+    fun textChanged(text:String){
+        _text.value=text
+        Log.d(TAG, "html ${text}")
+    }
+
+
+    enum class FontSize(val size: Int) {
+        SMALL(3),
+        NORMAL(6),
+        LARGE(7)
+    }
 }
