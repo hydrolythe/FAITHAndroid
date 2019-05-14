66f802cad2d7dc3a10a8dbeb8c64a125487f59f3
Karine Samyn <Karine.Samyn@hogent.be>
2019-05-14 11:55:17 +0200

    fix:overviewEventFragment espresso test

--- app/src/main/java/be/hogent/faith/faith/overviewEvents/OverviewEventsViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/overviewEvents/OverviewEventsViewModel.kt
@@ -1,7 +1,5 @@
 package be.hogent.faith.faith.overviewEvents
 
-import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
-import be.hogent.faith.domain.models.User
 
-class OverviewEventsViewModel(val user: LiveData<User>) : ViewModel()
+class OverviewEventsViewModel() : ViewModel()
