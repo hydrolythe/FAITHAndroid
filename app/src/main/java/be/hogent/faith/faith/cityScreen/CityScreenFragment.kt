fbf63709636af81b52ef63f4fa70d56b8ace8652
Karine Samyn <Karine.Samyn@hogent.be>
2019-06-13 16:58:59 +0200

    feat: add selected avatar to screen

--- app/src/main/java/be/hogent/faith/faith/cityScreen/CityScreenFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/cityScreen/CityScreenFragment.kt
@@ -10,8 +10,17 @@ import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import be.hogent.faith.R
 import be.hogent.faith.databinding.FragmentCityScreenBinding
+import be.hogent.faith.faith.UserViewModel
 import be.hogent.faith.faith.di.KoinModules
+import be.hogent.faith.faith.loginOrRegister.registerAvatar.AvatarProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import kotlinx.android.synthetic.main.fragment_city_screen.image_main_avatar
+import kotlinx.android.synthetic.main.fragment_enter_event_details.img_event_details_avatar_inkleuren
+import kotlinx.android.synthetic.main.fragment_enter_event_details.img_event_details_avatar_zittend
+import org.koin.android.ext.android.get
 import org.koin.android.ext.android.getKoin
+import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.viewModel
 
 /**
@@ -27,8 +36,10 @@ class CityScreenFragment : Fragment() {
 
     private var navigation: CityScreenNavigationListener? = null
     private val cityScreenViewModel: CityScreenViewModel by viewModel()
+    private val userViewModel: UserViewModel = get(scope = getKoin().getScope(KoinModules.USER_SCOPE_ID))
     private lateinit var mainScreenBinding: FragmentCityScreenBinding
     private lateinit var avatarView: View
+    private val avatarProvider: AvatarProvider by inject()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         mainScreenBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_city_screen, container, false)
@@ -55,6 +66,11 @@ class CityScreenFragment : Fragment() {
             // Close the user scope so a new one is created when logging in with another user
             getKoin().getScope(KoinModules.USER_SCOPE_ID).close()
             // TODO: go back to login Screen
+        })
+
+        userViewModel.user.observe(this, Observer{user ->
+            Glide.with(context!!).load(avatarProvider.getAvatarDrawableStaan(user.avatarName)).diskCacheStrategy(
+                DiskCacheStrategy.ALL).into(image_main_avatar)
         })
     }
 
