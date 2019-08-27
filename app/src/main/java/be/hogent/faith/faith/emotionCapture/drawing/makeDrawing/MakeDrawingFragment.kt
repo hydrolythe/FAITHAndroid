ee439357990340eb8b6c571dc8cd1d194fdeae77
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-08-27 11:05:35 +0200

    fix: drawing is gone after drawing emotionavatar
    Problem was that they both shared the same DrawingVM.
    Fixed by using a scope for the normal drawing and a shared VM for the
    EmotionAvatar.

--- app/src/main/java/be/hogent/faith/faith/emotionCapture/drawing/makeDrawing/MakeDrawingFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/emotionCapture/drawing/makeDrawing/MakeDrawingFragment.kt
@@ -1,6 +1,7 @@
 package be.hogent.faith.faith.emotionCapture.drawing.makeDrawing
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,13 +12,34 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import be.hogent.faith.R
 import be.hogent.faith.databinding.FragmentDrawBinding
+import be.hogent.faith.faith.di.KoinModules
 import be.hogent.faith.faith.emotionCapture.drawing.DrawFragment
+import be.hogent.faith.faith.emotionCapture.drawing.DrawViewModel
 import be.hogent.faith.faith.emotionCapture.enterEventDetails.EventViewModel
+import be.hogent.faith.util.TAG
 import com.divyanshu.draw.widget.DrawView
+import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.inject
 import org.koin.android.viewmodel.ext.android.sharedViewModel
+import org.koin.core.error.ScopeNotCreatedException
+import org.koin.core.qualifier.named
 
 class MakeDrawingFragment : DrawFragment() {
+
+    override val drawViewModel: DrawViewModel
+        get() {
+            Log.d(TAG, "Trying to get Drawing scope in MakeDrawing")
+            val scope = try {
+                getKoin().getScope(KoinModules.DRAWING_SCOPE_ID)
+            } catch (e: ScopeNotCreatedException) {
+                Log.d(TAG, "No Drawing scope available - Creating Drawing scope in MakeDrawing")
+                getKoin().createScope(
+                    KoinModules.DRAWING_SCOPE_ID,
+                    named(KoinModules.DRAWING_SCOPE_NAME)
+                )
+            }
+            return scope.get()
+        }
 
     private val eventViewModel: EventViewModel by sharedViewModel()
 
@@ -28,7 +50,11 @@ class MakeDrawingFragment : DrawFragment() {
 
     private lateinit var drawBinding: FragmentDrawBinding
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
         drawBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_draw, container, false)
         drawBinding.drawViewModel = drawViewModel
         drawBinding.lifecycleOwner = this
