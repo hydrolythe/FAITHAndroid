ff8f6ed4daa4a46566ccdb5917c84aaaa9d27975
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-08-28 13:11:26 +0200

    style

--- database/src/main/java/be/hogent/faith/database/daos/DetailDao.kt
+++ database/src/main/java/be/hogent/faith/database/daos/DetailDao.kt
@@ -4,7 +4,6 @@ import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import androidx.room.Transaction
 import be.hogent.faith.database.models.detail.AudioDetailEntity
 import be.hogent.faith.database.models.detail.DetailEntity
 import be.hogent.faith.database.models.detail.PictureDetailEntity
