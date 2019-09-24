6202ded1849af632650cc570e0c1d38af322179d
eothein <jens.buysse@gmail.com>
2019-09-24 13:19:58 +0200

    [refactor] Set background city park and adjust guide lines based on scaling

--- app/src/main/java/be/hogent/faith/faith/util/FragmentExtension.kt
+++ app/src/main/java/be/hogent/faith/faith/util/FragmentExtension.kt
@@ -1,10 +1,15 @@
 package be.hogent.faith.faith.util
 
+import android.util.Log
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.Guideline
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import be.hogent.faith.util.TAG
+import kotlinx.android.synthetic.main.fragment_city_screen.guide_pole_left_end
 
 /**
  * Returns a ViewModel of a certain type.
@@ -22,4 +27,23 @@ internal fun Fragment.replaceChildFragment(fragment: Fragment, frameId: Int) {
     ft.replace(frameId, fragment)
     ft.setTransition(FragmentTransaction.TRANSIT_FRAGMENT_FADE)
     ft.commit()
+}
+
+/**
+ * Adjusts the guideline (horizontally or vertically) based on the image which has been scaled.
+ */
+internal fun Fragment.adjustGuidelineAfterScaling(
+    guideline: Guideline,
+    padding: Float,
+    imageLength: Float,
+    screenLength: Float
+) {
+    // Calculate percentage after scaling
+    val originalPercentage =
+        (guideline.layoutParams as ConstraintLayout.LayoutParams).guidePercent
+    val newPercentage = ((originalPercentage * imageLength) + padding) / screenLength
+    guide_pole_left_end.setGuidelinePercent(newPercentage)
+
+    Log.i(TAG, "Changed ${guideline.id} from $originalPercentage % to $newPercentage")
+    Log.i(TAG, "Padding $padding, ImageLength $imageLength screenLength $screenLength")
 }
