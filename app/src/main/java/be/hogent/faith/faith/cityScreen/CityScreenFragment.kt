6a1abc069c97be4a1ecf07438383e417b6f914e6
eothein <jens.buysse@gmail.com>
2019-09-24 13:19:58 +0200

    [refactor] Set background city park and adjust guide lines based on scaling

--- app/src/main/java/be/hogent/faith/faith/cityScreen/CityScreenFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/cityScreen/CityScreenFragment.kt
@@ -1,10 +1,14 @@
 package be.hogent.faith.faith.cityScreen
 
 import android.content.Context
+import android.graphics.Matrix
+import android.graphics.Point
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewTreeObserver
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
@@ -14,9 +18,14 @@ import be.hogent.faith.faith.UserViewModel
 import be.hogent.faith.faith.di.KoinModules
 import be.hogent.faith.faith.loginOrRegister.LoginManager
 import be.hogent.faith.faith.loginOrRegister.registerAvatar.AvatarProvider
+import be.hogent.faith.faith.util.adjustGuidelineAfterScaling
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.engine.DiskCacheStrategy
 import kotlinx.android.synthetic.main.fragment_city_screen.background_city_screen
+import kotlinx.android.synthetic.main.fragment_city_screen.guide_loc2_bottom
+import kotlinx.android.synthetic.main.fragment_city_screen.guide_loc2_top
+import kotlinx.android.synthetic.main.fragment_city_screen.guide_pole_left_end
+import kotlinx.android.synthetic.main.fragment_city_screen.guide_pole_left_start
 import kotlinx.android.synthetic.main.fragment_city_screen.image_main_avatar
 import org.koin.android.ext.android.getKoin
 import org.koin.android.ext.android.inject
@@ -52,20 +61,57 @@ class CityScreenFragment : Fragment() {
         mainScreenBinding.cityScreenViewModel = cityScreenViewModel
         mainScreenBinding.lifecycleOwner = this
         avatarView = mainScreenBinding.imageMainAvatar
+
         return mainScreenBinding.root
     }
 
     override fun onStart() {
         super.onStart()
+
         registerListeners()
 
-        setBackgroundImage()
-    }
+        // We need to get the screen dimensions to set the guide lines correct
+        val display = activity?.windowManager?.defaultDisplay
+        val size = Point()
+        display?.getSize(size)
+        val screenWidth = size.x.toFloat()
+        val screenHeight = size.y.toFloat()
+        Log.e("Width", "" + screenWidth)
+        Log.e("height", "" + screenHeight)
 
-    private fun setBackgroundImage() {
-        Glide.with(requireContext())
-            .load(R.drawable.citymap_faith)
-            .into(background_city_screen)
+        // We wait for the image to be drawn and scaled, get its dimensions and then
+        // set the guidlines properly
+        background_city_screen.viewTreeObserver.addOnGlobalLayoutListener(object :
+            ViewTreeObserver.OnGlobalLayoutListener {
+            override fun onGlobalLayout() {
+
+                // Remove the listener otherwise infinite loop
+                background_city_screen.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+                // Get the width & height of the background image after scaling
+                val f = FloatArray(9)
+                background_city_screen.imageMatrix.getValues(f)
+                val drawableHeight = background_city_screen.height.toFloat()
+                val drawableWidth = background_city_screen.width.toFloat()
+                val displayedWidth = drawableWidth * f[Matrix.MSCALE_X]
+                val displayedHeight = drawableHeight * f[Matrix.MSCALE_Y]
+
+                // Calculate "padding"
+                val left = (screenWidth - displayedWidth) / 2
+                val top = (screenHeight - displayedHeight) / 2
+
+                // Adjust the guide lines
+                adjustGuidelineAfterScaling(guide_pole_left_end, left, displayedWidth, screenWidth)
+                adjustGuidelineAfterScaling(
+                    guide_pole_left_start,
+                    left,
+                    displayedWidth,
+                    screenWidth
+                )
+                adjustGuidelineAfterScaling(guide_loc2_bottom, top, displayedHeight, screenHeight)
+                adjustGuidelineAfterScaling(guide_loc2_top, top, displayedHeight, screenHeight)
+            }
+        })
     }
 
     private fun registerListeners() {
