0c24bb6f0f0920bfa18de4c99bc8c37fc2670602
eothein <jens.buysse@gmail.com>
2019-10-07 17:00:21 +0200

    [layout] Create card layout for emotion recyclerview

---
+++ app/src/main/java/be/hogent/faith/faith/library/EventListActivity.kt
@@ -0,0 +1,107 @@
+package be.hogent.faith.faith.library
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.RecyclerView
+import be.hogent.faith.R
+import be.hogent.faith.faith.library.dummy.DummyContent
+import kotlinx.android.synthetic.main.event_list.event_detail_container
+import kotlinx.android.synthetic.main.event_list.event_list
+
+/**
+ * An activity representing a list of Pings. This activity
+ * has different presentations for handset and tablet-size devices. On
+ * handsets, the activity presents a list of items, which when touched,
+ * lead to a [EventDetailActivity] representing
+ * item details. On tablets, the activity presents the list of items and
+ * item details side-by-side using two vertical panes.
+ */
+class EventListActivity : AppCompatActivity() {
+
+    /**
+     * Whether or not the activity is in two-pane mode, i.e. running on a tablet
+     * device.
+     */
+    private var twoPane: Boolean = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_event_list)
+
+
+
+        if (event_detail_container != null) {
+            // The detail container view will be present only in the
+            // large-screen layouts (res/values-w900dp).
+            // If this view is present, then the
+            // activity should be in two-pane mode.
+            twoPane = true
+        }
+
+        setupRecyclerView(event_list)
+    }
+
+    private fun setupRecyclerView(recyclerView: RecyclerView) {
+        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
+    }
+
+    class SimpleItemRecyclerViewAdapter(
+        private val parentActivity: EventListActivity,
+        private val values: List<DummyContent.DummyItem>,
+        private val twoPane: Boolean
+    ) :
+        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
+
+        private val onClickListener: View.OnClickListener
+
+        init {
+            onClickListener = View.OnClickListener { v ->
+                val item = v.tag as DummyContent.DummyItem
+                if (twoPane) {
+                    val fragment = EventDetailFragment().apply {
+                        arguments = Bundle().apply {
+                            putString(EventDetailFragment.ARG_ITEM_ID, item.id)
+                        }
+                    }
+                    parentActivity.supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.event_detail_container, fragment)
+                        .commit()
+                } else {
+                    val intent = Intent(v.context, EventDetailActivity::class.java).apply {
+                        putExtra(EventDetailFragment.ARG_ITEM_ID, item.id)
+                    }
+                    v.context.startActivity(intent)
+                }
+            }
+        }
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+            val view = LayoutInflater.from(parent.context)
+                .inflate(R.layout.event_list_content, parent, false)
+            return ViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+            val item = values[position]
+            //holder.idView.text = item.id
+            //holder.contentView.text = item.content
+
+            with(holder.itemView) {
+                tag = item
+                setOnClickListener(onClickListener)
+            }
+        }
+
+        override fun getItemCount() = values.size
+
+        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            //val idView: TextView = view.id_text
+            //val contentView: TextView = view.content
+        }
+    }
+}
