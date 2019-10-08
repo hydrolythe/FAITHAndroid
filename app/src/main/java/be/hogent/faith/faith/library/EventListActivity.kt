48bfb28808574abca71a5e15ea02dbf9af0ee697
eothein <jens.buysse@gmail.com>
2019-10-08 11:23:00 +0200

    [feat] Add recyclerview data to overview events

--- app/src/main/java/be/hogent/faith/faith/library/EventListActivity.kt
+++ app/src/main/java/be/hogent/faith/faith/library/EventListActivity.kt
@@ -1,26 +1,39 @@
 package be.hogent.faith.faith.library
 
-import android.content.Intent
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import be.hogent.faith.R
-import be.hogent.faith.faith.library.dummy.DummyContent
+import be.hogent.faith.domain.models.User
+import be.hogent.faith.faith.UserViewModel
+import be.hogent.faith.faith.di.KoinModules
+import be.hogent.faith.faith.library.eventList.EventListener
+import be.hogent.faith.faith.library.eventList.EventsAdapter
+import com.bumptech.glide.Glide
 import kotlinx.android.synthetic.main.event_list.event_detail_container
-import kotlinx.android.synthetic.main.event_list.event_list
+import kotlinx.android.synthetic.main.event_list.rv_eventlist
+import org.koin.android.ext.android.getKoin
+import java.util.UUID
 
 /**
- * An activity representing a list of Pings. This activity
- * has different presentations for handset and tablet-size devices. On
- * handsets, the activity presents a list of items, which when touched,
+ * An activity representing a list of Events of the user. This activity
+ * has different presentations for small and bigger devices. On
+ * smaller devices, the activity presents a list of events, which when touched,
  * lead to a [EventDetailActivity] representing
- * item details. On tablets, the activity presents the list of items and
- * item details side-by-side using two vertical panes.
+ * event details. On nbigger devices, the activity presents the list of events and
+ * events details side-by-side using two vertical panes.
  */
 class EventListActivity : AppCompatActivity() {
+
+
+    private val userViewModel: UserViewModel = getKoin().getScope(KoinModules.USER_SCOPE_ID).get()
+
+    private lateinit var eventsAdapter: EventsAdapter
+
+    private lateinit var eventListener: EventListener
 
     /**
      * Whether or not the activity is in two-pane mode, i.e. running on a tablet
@@ -32,8 +45,6 @@ class EventListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_event_list)
 
-
-
         if (event_detail_container != null) {
             // The detail container view will be present only in the
             // large-screen layouts (res/values-w900dp).
@@ -41,67 +52,38 @@ class EventListActivity : AppCompatActivity() {
             // activity should be in two-pane mode.
             twoPane = true
         }
+        setupRecyclerView(rv_eventlist)
+    }
 
-        setupRecyclerView(event_list)
+    override fun onStart() {
+        super.onStart()
+        setupRecyclerView(rv_eventlist)
+        startListeners()
     }
 
     private fun setupRecyclerView(recyclerView: RecyclerView) {
-        recyclerView.adapter = SimpleItemRecyclerViewAdapter(this, DummyContent.ITEMS, twoPane)
+        eventListener = object : EventListener {
+            override fun onEventClicked(eventUuid: UUID) {
+                Log.i("Tag", "Clicked")
+            }
+        }
+        eventsAdapter = EventsAdapter(eventListener, Glide.with(this))
+        recyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            this.adapter = eventsAdapter
+        }
     }
 
-    class SimpleItemRecyclerViewAdapter(
-        private val parentActivity: EventListActivity,
-        private val values: List<DummyContent.DummyItem>,
-        private val twoPane: Boolean
-    ) :
-        RecyclerView.Adapter<SimpleItemRecyclerViewAdapter.ViewHolder>() {
-
-        private val onClickListener: View.OnClickListener
-
-        init {
-            onClickListener = View.OnClickListener { v ->
-                val item = v.tag as DummyContent.DummyItem
-                if (twoPane) {
-                    val fragment = EventDetailFragment().apply {
-                        arguments = Bundle().apply {
-                            putString(EventDetailFragment.ARG_ITEM_ID, item.id)
-                        }
-                    }
-                    parentActivity.supportFragmentManager
-                        .beginTransaction()
-                        .replace(R.id.event_detail_container, fragment)
-                        .commit()
-                } else {
-                    val intent = Intent(v.context, EventDetailActivity::class.java).apply {
-                        putExtra(EventDetailFragment.ARG_ITEM_ID, item.id)
-                    }
-                    v.context.startActivity(intent)
+    private fun startListeners() {
+        userViewModel.user.observe(this, Observer<User> { user ->
+            user?.let {
+                eventsAdapter.apply {
+                    events = user.events
+                    notifyDataSetChanged()
                 }
             }
-        }
-
-        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-            val view = LayoutInflater.from(parent.context)
-                .inflate(R.layout.event_list_content, parent, false)
-            return ViewHolder(view)
-        }
-
-        override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-            val item = values[position]
-            //holder.idView.text = item.id
-            //holder.contentView.text = item.content
-
-            with(holder.itemView) {
-                tag = item
-                setOnClickListener(onClickListener)
-            }
-        }
-
-        override fun getItemCount() = values.size
-
-        inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
-            //val idView: TextView = view.id_text
-            //val contentView: TextView = view.content
-        }
+        })
     }
+
+
 }
