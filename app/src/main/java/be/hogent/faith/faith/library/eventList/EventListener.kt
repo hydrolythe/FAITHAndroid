a1a83dbd4bbd32dc6365d28005a5bae4761056c0
eothein <jens.buysse@gmail.com>
2019-10-08 11:23:00 +0200

    [feat] Add recyclerview data to overview events

---
+++ app/src/main/java/be/hogent/faith/faith/library/eventList/EventListener.kt
@@ -0,0 +1,7 @@
+package be.hogent.faith.faith.library.eventList
+
+import java.util.UUID
+
+interface EventListener {
+    fun onEventClicked(eventUuid: UUID)
+}
---
+++ app/src/main/java/be/hogent/faith/faith/library/eventList/EventsAdapter.kt
@@ -0,0 +1,62 @@
+package be.hogent.faith.faith.library.eventList
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import be.hogent.faith.R
+import be.hogent.faith.domain.models.Event
+import com.bumptech.glide.RequestManager
+import com.bumptech.glide.request.RequestOptions
+import org.threeten.bp.format.DateTimeFormatter
+
+class EventsAdapter(private val eventListener: EventListener, private val glide: RequestManager) :
+    RecyclerView.Adapter<EventsAdapter.ViewHolder>() {
+
+    var events: List<Event> = emptyList()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            LayoutInflater
+                .from(parent.context)
+                .inflate(R.layout.event_list_content, parent, false)
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return events.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(events[position])
+    }
+
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+
+        private var avatarImage: ImageView = view.findViewById(R.id.img_avatar)
+        private var eventTitle: TextView = view.findViewById(R.id.lbl_title)
+        private var eventDate: TextView = view.findViewById(R.id.lbl_date)
+        private var eventDesc: TextView = view.findViewById(R.id.lbl_description)
+
+        fun bind(event: Event) {
+            eventTitle.text = event.title
+            val formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy")
+            val eventDateString: String = formatter.format(event.dateTime)
+            eventDate.text = eventDateString
+
+            event.emotionAvatar?.let {
+                glide
+                    .load(it)
+                    .apply(RequestOptions.circleCropTransform())
+                    .into(avatarImage)
+            }
+
+            //itemView.setOnClickListener {
+            //    eventListener.onEventClicked(event.uuid)
+            //}
+        }
+    }
+}
