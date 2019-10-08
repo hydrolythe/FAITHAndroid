0f30af5fb566358dcfa9fbb374abb889491adf34
eothein <jens.buysse@gmail.com>
2019-10-08 16:46:45 +0200

    [feat] Add viewpager to second pane layout

--- app/src/main/java/be/hogent/faith/faith/library/EventDetailFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/library/EventDetailFragment.kt
@@ -5,7 +5,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import androidx.viewpager2.widget.ViewPager2
+
 import be.hogent.faith.R
+import be.hogent.faith.faith.library.eventDetailFragments.TestFragment
 
 
 /**
@@ -16,15 +20,30 @@ import be.hogent.faith.R
  */
 class EventDetailFragment : Fragment() {
 
+    private lateinit var mPager: ViewPager2
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         val rootView = inflater.inflate(R.layout.fragment_eventdetails, container, false)
-
-
+        mPager = rootView.findViewById(R.id.pager_eventdetails)
+        val pagerAdapter = ScreenSlidePagerAdapter(this)
+        mPager.adapter = pagerAdapter
         return rootView
+    }
+
+
+    /**
+     * A simple pager adapter that represents 5 ScreenSlidePageFragment objects, in
+     * sequence.
+     */
+    private inner class ScreenSlidePagerAdapter(fa: Fragment) : FragmentStateAdapter(fa) {
+        override fun getItemCount(): Int = 5
+
+        override fun createFragment(position: Int): Fragment {
+            return TestFragment.newInstance()
+        }
     }
 
 
