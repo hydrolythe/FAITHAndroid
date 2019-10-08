f28e8c20a84cf225d317826bb91d9968abf94935
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-10-08 12:06:29 +0200

    feat: show timer when recording

--- app/src/main/java/be/hogent/faith/faith/emotionCapture/recordAudio/AudioViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/emotionCapture/recordAudio/AudioViewModel.kt
@@ -2,12 +2,15 @@ package be.hogent.faith.faith.emotionCapture.recordAudio
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import be.hogent.faith.domain.models.detail.AudioDetail
 import be.hogent.faith.faith.emotionCapture.recordAudio.audioStates.AudioContext
 import be.hogent.faith.faith.emotionCapture.recordAudio.audioStates.AudioState
 import be.hogent.faith.faith.emotionCapture.recordAudio.audioStates.playState.PlayStateInitial
 import be.hogent.faith.faith.emotionCapture.recordAudio.audioStates.recordState.RecordStateInitial
+import java.util.Timer
+import kotlin.concurrent.timer
 
 class AudioViewModel : ViewModel(), AudioContext {
     private val _audioState = MutableLiveData<AudioState>()
@@ -17,6 +20,8 @@ class AudioViewModel : ViewModel(), AudioContext {
     private val _existingDetail = MutableLiveData<AudioDetail>()
 
     override var finishedRecording = false
+
+    private var recordingTimer: Timer? = null
 
     /**
      *
@@ -35,9 +40,19 @@ class AudioViewModel : ViewModel(), AudioContext {
     val saveButtonVisible: LiveData<Boolean>
         get() = _saveButtonVisible
 
+    val recordingTimeVisibility = MutableLiveData<Int>()
+
+    private val _recordingTime = MutableLiveData<Int>()
+    val recordingTimeString: LiveData<String> = Transformations.map(_recordingTime) { time ->
+        val minutesPadded = (time / 60).toString().padStart(2, '0')
+        val secondsPadded = (time % 60).toString().padStart(2, '0')
+        return@map "$minutesPadded:$secondsPadded"
+    }
+
     init {
         // Can be set to false once an existing Detail is added
         _saveButtonVisible.value = true
+        _recordingTime.value = 0
     }
 
     /**
@@ -70,22 +85,31 @@ class AudioViewModel : ViewModel(), AudioContext {
                 _audioState.value?.recordButtonEnabled
             }
         _saveButtonVisible.value = finishedRecording
+
+        recordingTimeVisibility.value = _audioState.value?.recordingTimeVisibility
     }
 
     fun onRecordButtonClicked() {
         _audioState.value?.onRecordPressed()
+        recordingTimer = timer(initialDelay = 1000, period = 1000) {
+            _recordingTime.postValue(_recordingTime.value!! + 1)
+        }
     }
 
     fun onStopButtonClicked() {
         _audioState.value?.onStopPressed()
+        recordingTimer?.cancel()
+        _recordingTime.postValue(0)
     }
 
     fun onPauseButtonClicked() {
         _audioState.value?.onPausePressed()
+        recordingTimer?.cancel()
     }
 
     fun onPlayButtonClicked() {
         _audioState.value?.onPlayPressed()
+        recordingTimer?.cancel()
     }
 
     override fun onCleared() {
