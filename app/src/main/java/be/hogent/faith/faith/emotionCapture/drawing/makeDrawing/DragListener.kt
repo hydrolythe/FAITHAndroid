d011e710b16c8485999198394d7df1fe33785b53
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-10-21 11:40:20 +0200

    refactor: move to Timber

--- app/src/main/java/be/hogent/faith/faith/emotionCapture/drawing/makeDrawing/DragListener.kt
+++ app/src/main/java/be/hogent/faith/faith/emotionCapture/drawing/makeDrawing/DragListener.kt
@@ -1,11 +1,11 @@
 package be.hogent.faith.faith.emotionCapture.drawing.makeDrawing
 
-import android.util.Log
 import android.view.DragEvent
 import android.view.View
 import android.view.View.OnDragListener
 import be.hogent.faith.util.TAG
 import com.divyanshu.draw.widget.DrawView
+import timber.log.Timber
 import kotlin.math.roundToInt
 
 class DragListener(private val drawView: DrawView) : OnDragListener {
@@ -13,13 +13,17 @@ class DragListener(private val drawView: DrawView) : OnDragListener {
     override fun onDrag(v: View, event: DragEvent): Boolean {
         when (event.action) {
             DragEvent.ACTION_DRAG_STARTED -> {
-                Log.d(TAG, "Started dragging")
+                Timber.d(TAG, "Started dragging")
             }
             DragEvent.ACTION_DROP -> {
-                Log.d(TAG, "Stopped dragging, dropped at x: ${event.x}, y: ${event.y}")
-                Log.d(TAG, "event: ${event.x}, y: ${event.y}")
+                Timber.d(TAG, "Stopped dragging, dropped at x: ${event.x}, y: ${event.y}")
+                Timber.d(TAG, "event: ${event.x}, y: ${event.y}")
 
-                drawView.addDrawable(Integer.parseInt((event.clipData.getItemAt(0).text).toString()), event.x.roundToInt(), event.y.roundToInt())
+                drawView.addDrawable(
+                    Integer.parseInt((event.clipData.getItemAt(0).text).toString()),
+                    event.x.roundToInt(),
+                    event.y.roundToInt()
+                )
             }
         }
         return true // Keep listening to drag events
