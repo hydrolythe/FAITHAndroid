8bc1369093baefac574935614e9b0cc0d60e6549
Harm De Weirdt <harm.deweirdt@hogent.be>
2019-11-27 15:19:30 +0100

    test: use correct observers in test

--- app/src/androidTest/java/be/hogent/faith/faith/util/TextinputErrorMatcher.kt
+++ app/src/androidTest/java/be/hogent/faith/faith/util/TextinputErrorMatcher.kt
@@ -6,9 +6,8 @@ import org.hamcrest.Description
 import org.hamcrest.Matcher
 import org.hamcrest.TypeSafeMatcher
 
-
-fun hasTextInputLayoutHintText(expectedError : String) : Matcher<View> {
-    return object: TypeSafeMatcher<View>(){
+fun hasTextInputLayoutHintText(expectedError: String): Matcher<View> {
+    return object : TypeSafeMatcher<View>() {
         override fun describeTo(description: Description?) {
             description?.appendText("should be error : $expectedError or of type TextInputLayout")
         }
@@ -18,14 +17,12 @@ fun hasTextInputLayoutHintText(expectedError : String) : Matcher<View> {
          * expected error.
          */
         override fun matchesSafely(item: View?): Boolean {
-            if( !(item is TextInputLayout)) {
+            if (!(item is TextInputLayout)) {
                 return false
             }
             val error = (item as TextInputLayout).error ?: return false
             val hint = error.toString()
             return expectedError == hint
         }
-
-
     }
 }
