9c8e897b3c15e0bc8cfbdeef25efd0a7e61612aa
SWojtyla <sebastien.wojtyla.y3274@student.hogent.be>
2020-03-19 16:47:01 +0100

    Searchbar is not case sensitive

--- app/src/main/java/be/hogent/faith/faith/backpackScreen/detailFilters/DetailNameFilter.kt
+++ app/src/main/java/be/hogent/faith/faith/backpackScreen/detailFilters/DetailNameFilter.kt
@@ -1,13 +1,14 @@
 package be.hogent.faith.faith.backpackScreen.detailFilters
 
 import be.hogent.faith.domain.models.detail.Detail
+import java.util.*
 
 class DetailNameFilter(var searchString: String) : DetailFilter {
 
     override fun invoke(detail: Detail): Boolean {
         val searchWords = searchString.split(" ")
         return searchWords.all {
-            detail.fileName.contains(it)
+            detail.fileName.toLowerCase(Locale.ROOT).contains(it.toLowerCase(Locale.ROOT))
         }
     }
 }
