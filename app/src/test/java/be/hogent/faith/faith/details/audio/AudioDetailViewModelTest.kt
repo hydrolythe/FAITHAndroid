9df2355ea89bcf0a8263d36b9e1c1267a865b924
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-03-24 13:34:15 +0100

    update travis config

--- app/src/test/java/be/hogent/faith/faith/details/audio/AudioDetailViewModelTest.kt
+++ app/src/test/java/be/hogent/faith/faith/details/audio/AudioDetailViewModelTest.kt
@@ -7,9 +7,9 @@ import be.hogent.faith.faith.di.appModule
 import be.hogent.faith.faith.testModule
 import be.hogent.faith.service.usecases.detail.audioDetail.CreateAudioDetailUseCase
 import io.mockk.mockk
-import junit.framework.Assert.assertEquals
 import org.junit.After
 import org.junit.Assert.assertFalse
+import org.junit.Assert.assertEquals
 import org.junit.Assert.assertTrue
 import org.junit.Before
 import org.junit.Rule
