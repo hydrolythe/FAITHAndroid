8e1bd16452c8ff3b45592a8cea54bf354c197ec5
SWojtyla <sebastien.wojtyla.y3274@student.hogent.be>
2020-04-01 11:15:37 +0200

    Fragment_cinema_start ui+rv

--- app/src/main/java/be/hogent/faith/faith/cinema/CinemaActivity.kt
+++ app/src/main/java/be/hogent/faith/faith/cinema/CinemaActivity.kt
@@ -3,8 +3,10 @@ package be.hogent.faith.faith.cinema
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import be.hogent.faith.R
+import be.hogent.faith.domain.models.detail.Detail
+import be.hogent.faith.faith.emotionCapture.enterEventDetails.DetailViewHolder
 
-class CinemaActivity : AppCompatActivity(),CinemaStartScreenFragment.CinemaNavigationListener {
+class CinemaActivity : AppCompatActivity(),CinemaStartScreenFragment.CinemaNavigationListener, DetailViewHolder.ExistingDetailNavigationListener {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +20,23 @@ class CinemaActivity : AppCompatActivity(),CinemaStartScreenFragment.CinemaNavig
         }
     }
 
+    override fun startPhotoDetailFragment() {
+        TODO("Not yet implemented")
+    }
+
+    override fun startDrawingDetailFragment() {
+        TODO("Not yet implemented")
+    }
+
     override fun closeCinema() {
         finish()
+    }
+
+    override fun openDetailScreenFor(detail: Detail) {
+        TODO("Not yet implemented")
+    }
+
+    override fun deleteDetail(detail: Detail) {
+        TODO("Not yet implemented")
     }
 }
--- app/src/main/java/be/hogent/faith/faith/cinema/CinemaStartScreenFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/cinema/CinemaStartScreenFragment.kt
@@ -1,27 +1,89 @@
 package be.hogent.faith.faith.cinema
 
+import android.content.Context
 import android.os.Bundle
+import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.PopupMenu
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
 import be.hogent.faith.R
 import be.hogent.faith.databinding.FragmentCinemaStartBinding
-import be.hogent.faith.faith.library.eventList.EventListFragment
+import be.hogent.faith.faith.emotionCapture.enterEventDetails.DetailThumbnailsAdapter
+import timber.log.Timber
 
 class CinemaStartScreenFragment : Fragment() {
 
 
+    private var navigation: CinemaNavigationListener? = null
+    private var detailThumbnailsAdapter: DetailThumbnailsAdapter? = null
+    private lateinit var addDetailMenu: PopupMenu
     private lateinit var binding: FragmentCinemaStartBinding
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_cinema_start, container, false)
         binding.lifecycleOwner = this
+
+
         return binding.root
 
     }
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+        if (context is CinemaNavigationListener) {
+            navigation = context
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        startListeners()
+        updateUI()
+    }
+    private fun updateUI() {
+        detailThumbnailsAdapter = DetailThumbnailsAdapter(
+                emptyList(),
+                requireNotNull(activity) as CinemaActivity
+        )
+        binding.rvCinema.layoutManager = GridLayoutManager(activity, 6)
+        binding.rvCinema.adapter = detailThumbnailsAdapter
+    }
+    private fun startListeners() {
+
+        binding.btnDetails.setOnClickListener {
+            binding.btnCinemaAdd.visibility = View.VISIBLE
+        }
+        binding.btnCinemaAdd.setOnClickListener {
+            addDetailMenu = PopupMenu(binding.btnCinemaAdd.context, binding.btnCinemaAdd, Gravity.TOP, 0, R.style.PopupMenu_AddDetail)
+            addDetailMenu.gravity = Gravity.TOP
+            addDetailMenu.menuInflater.inflate(R.menu.menu_cinema, addDetailMenu.menu)
+
+            try {
+                val fieldMPopup = PopupMenu::class.java.getDeclaredField("mPopup")
+                fieldMPopup.isAccessible = true
+                val mPopup = fieldMPopup.get(addDetailMenu)
+                mPopup.javaClass
+                        .getDeclaredMethod("setForceShowIcon", Boolean::class.java)
+                        .invoke(mPopup, true)
+            } catch (e: Exception) {
+                Timber.e("Error showing icons")
+            }
+            addDetailMenu.show()
+        }
+
+        binding.btnFilms.setOnClickListener{
+            binding.btnCinemaAdd.visibility = View.GONE
+        }
+        binding.btnCinemaCancel.setOnClickListener{
+            navigation!!.closeCinema()
+        }
+    }
+
     companion object {
         fun newInstance(): CinemaStartScreenFragment {
             return CinemaStartScreenFragment()
@@ -29,6 +91,8 @@ class CinemaStartScreenFragment : Fragment() {
     }
 
     interface CinemaNavigationListener {
+        fun startPhotoDetailFragment()
+        fun startDrawingDetailFragment()
         fun closeCinema()
     }
 
