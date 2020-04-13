e0155bdfceae0afe55bddf60d5b5c7fbe7cab4a8
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-04-13 10:08:13 +0200

    refactor: Event is no longer a detailsContainer (see kdoc)

--- domain/src/main/java/be/hogent/faith/domain/models/DetailsContainer.kt
+++ domain/src/main/java/be/hogent/faith/domain/models/DetailsContainer.kt
@@ -3,6 +3,14 @@ package be.hogent.faith.domain.models
 import be.hogent.faith.domain.models.detail.Detail
 import java.util.UUID
 
+/**
+ * Represents a container of details (duh).
+ * The backpack, cinema,... all share the functionality of displaying a collection of details.
+ *
+ * The event is different from the DetailsContainer because there are multiple events, but there
+ * is always only one Backpack, Cinema,... This has consequences from the service layer and beyond,
+ * where for example encryption and storage has to be handled differently.
+ */
 sealed class DetailsContainer {
 
     private val _details = mutableListOf<Detail>()
@@ -23,3 +31,23 @@ sealed class DetailsContainer {
 }
 
 class Backpack : DetailsContainer()
+
+class Cinema : DetailsContainer() {
+
+    private val _films = mutableListOf<Film>()
+    val films: List<Film>
+        get() = _films
+
+    fun getFilm(uuid: UUID): Film? {
+        return films.find { it.uuid == uuid }
+    }
+
+    fun addFilm(film: Film) {
+        // TODO : validatie toevoegen : duration - naam - datum
+        _films += film
+    }
+
+    fun removeFilm(film: Film) {
+        _films.remove(film)
+    }
+}
