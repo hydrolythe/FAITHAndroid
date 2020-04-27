008bd546c57d775af3161da5752332d09303ac19
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-04-27 16:06:17 +0200

    update all dependencies

--- service/src/main/java/be/hogent/faith/service/network/YoutubeService.kt
+++ service/src/main/java/be/hogent/faith/service/network/YoutubeService.kt
@@ -2,9 +2,9 @@ package be.hogent.faith.service.network
 
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import io.reactivex.Single
 import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import retrofit2.http.Query
