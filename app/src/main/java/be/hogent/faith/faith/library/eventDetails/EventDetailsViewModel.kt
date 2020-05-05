44d10928b95eaf7ac2131ce5a22ff2969ac8da1e
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-05-05 17:17:37 +0200

    show event data immediately when opening event detail

--- app/src/main/java/be/hogent/faith/faith/library/eventDetails/EventDetailsViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/library/eventDetails/EventDetailsViewModel.kt
@@ -35,6 +35,9 @@ class EventDetailsViewModel(
     val cancelButtonClicked: LiveData<Unit> = _cancelButtonClicked
 
     fun setEvent(event: Event) {
+        // Set the event already so at least the data is shown
+        _event.value = event
+        // Then start the use case so files will be loaded
         makeEventFilesAvailableUseCase.execute(
             MakeEventFilesAvailableUseCase.Params(event),
             object :
