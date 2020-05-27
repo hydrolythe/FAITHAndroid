a535be14483e474b80330f6751cec8d4cf558cc1
eliasgeee <elias.gryp.w0050@student.hogent.be>
2020-05-27 10:06:16 +0200

    goalfactory

--- util/src/main/java/be/hogent/faith/util/factory/GoalFactory.kt
+++ util/src/main/java/be/hogent/faith/util/factory/GoalFactory.kt
@@ -3,12 +3,22 @@ package be.hogent.faith.util.factory
 import be.hogent.faith.domain.models.goals.Goal
 import be.hogent.faith.domain.models.goals.SkyscraperType
 
-/*
+
 object GoalFactory {
-    fun makeGoal(numberOfSubGoals: Int = 5): Goal {
+    fun makeGoal(skyscraperType: SkyscraperType, numberOfSubGoals: Int = 5): Goal {
         val goal = Goal(
             description = DataFactory.randomString(),
-            skyscraperType = SkyscraperType.Skyscraper_1
+            skyscraperType = skyscraperType
         )
+        return goal
     }
-}*/
+
+    fun makeGoalsList(count : Int = 5) : List<Goal> {
+        val types = SkyscraperType.values()
+        val goals = mutableListOf<Goal>()
+        repeat(count){
+            makeGoal(types.get(it))
+        }
+        return goals
+    }
+}
