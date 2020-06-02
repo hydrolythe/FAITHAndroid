bd51aa9943909e98cdb8932aa9fa5d499361440c
SWojtyla <sebastien.wojtyla.y3274@student.hogent.be>
2020-06-02 08:41:57 +0200

    Drag and drop actions in rv

--- app/src/main/java/be/hogent/faith/faith/skyscraper/goal/SkyscraperGoalFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/skyscraper/goal/SkyscraperGoalFragment.kt
@@ -8,12 +8,15 @@ import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import be.hogent.faith.R
 import be.hogent.faith.databinding.FragmentSkyscraperGoalBinding
 import be.hogent.faith.faith.UserViewModel
 import be.hogent.faith.faith.di.KoinModules
 import org.koin.android.ext.android.getKoin
+import java.util.Collections
 
 class SkyscraperGoalFragment : Fragment() {
 
@@ -23,7 +26,7 @@ class SkyscraperGoalFragment : Fragment() {
     private lateinit var avatarOnDragListener: AvatarOnDragListener
     private lateinit var avatarOnTouchListener: AvatarOnTouchListener
     private lateinit var adapter: ActionAdapter
-
+    val list = arrayListOf<Action>()
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -56,12 +59,31 @@ class SkyscraperGoalFragment : Fragment() {
 
             }
         }
+        val touchHelper = ItemTouchHelper(object : ItemTouchHelper.SimpleCallback(ItemTouchHelper.UP or ItemTouchHelper.DOWN,0){
+            override fun onMove(
+                recyclerView: RecyclerView,
+                viewHolder: RecyclerView.ViewHolder,
+                target: RecyclerView.ViewHolder
+            ): Boolean {
+                val sourcePosition = viewHolder.adapterPosition
+                val targetPosition = target.adapterPosition
+                Collections.swap(list,sourcePosition,targetPosition)
+                adapter.notifyItemMoved(sourcePosition,targetPosition)
+                return true
+            }
+
+            override fun onSwiped(viewHolder: RecyclerView.ViewHolder, direction: Int) {
+                // Niet van toepassing
+            }
+
+        })
+        touchHelper.attachToRecyclerView(binding.rvGoalActions)
         adapter = ActionAdapter(actionListener)
         binding.rvGoalActions.layoutManager =
             LinearLayoutManager(requireContext(), LinearLayoutManager.VERTICAL, false)
         binding.rvGoalActions.adapter = adapter
 
-        val list = arrayListOf<Action>()
+
         list.add(Action("Dit is een eerste actie"))
         list.add(Action("Dit is een tweede actie"))
         list.add(Action("Dit is een derde actie"))
@@ -84,6 +106,10 @@ class SkyscraperGoalFragment : Fragment() {
         binding.skyscraperAvatarDragDrop.avatarPosStairs5.setOnDragListener(avatarOnDragListener)
         binding.skyscraperAvatarDragDrop.avatarPosStairs6.setOnDragListener(avatarOnDragListener)
 
+        binding.btnAddAction.setOnClickListener{
+            list.add(Action("Dit is een actie"))
+            adapter.updateActionsList(list)
+        }
     }
 
     override fun onAttach(context: Context) {
