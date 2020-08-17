b8f7b7171d55ca0db713f5f8b9db20b1032676e5
Harm De Weirdt <harm.deweirdt@hogent.be>
2020-08-17 12:15:15 +0200

    add documentation to webp conversion script

---
+++ scripts/README.md
@@ -0,0 +1,5 @@
+# convertDrawablesToWebP
+
+Wordt gebruikt om alle png drawables te converteren naar webp met behulp van cwebp.exe (in deze folder te vinden).
+Deze tool is ook te vinden online: https://developers.google.com/speed/webp/download
+Het script moet uitgevoerd met Powershell worden vanuit de scripts folder zelf.
--- scripts/convertDrawablesToWebP.ps1
+++ scripts/convertDrawablesToWebP.ps1
@@ -1,4 +1,4 @@
-$pngs = Get-ChildItem -Recurse -Path "C:\Users\FAITH\AndroidStudioProjects\FAITH\app\src\main\res\drawable*" | where {$_.extension -eq ".png"}
+$pngs = Get-ChildItem -Recurse -Path "..\app\src\main\res\drawable*" | where { $_.extension -eq ".png" }
 foreach  ($png in $pngs) {
  .\cwebp.exe -lossless -mt $png -o "$(Join-Path $png.DirectoryName $png.BaseName).webp"
 }
