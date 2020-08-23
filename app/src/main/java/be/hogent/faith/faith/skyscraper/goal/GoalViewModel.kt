3b994c1905e157f81757087e8365a4b7a61c285e
Karine Samyn <Karine.Samyn@hogent.be>
2020-08-23 20:27:37 +0200

    new images for avatar

--- app/src/main/java/be/hogent/faith/faith/skyscraper/goal/GoalViewModel.kt
+++ app/src/main/java/be/hogent/faith/faith/skyscraper/goal/GoalViewModel.kt
@@ -81,7 +81,7 @@ class GoalViewModel(
         selectedSubGoalDescription.value = selectedSubGoal.value!!.second.description
     }
 
-    private fun updateCurrentSelectedSubGoal() {
+    fun updateCurrentSelectedSubGoal() {
         selectedSubGoal.value?.let {
             it.second.description = selectedSubGoalDescription.value!!
             selectedSubGoal.value = selectedSubGoal.value
@@ -108,6 +108,7 @@ class GoalViewModel(
             if (selectedSubGoalDescription.value.isNullOrEmpty())
                 _errorMessage.value = R.string.subdoel_naam_verplicht
             else {
+                updateCurrentSelectedSubGoal()
                 selectedSubGoal.value?.second?.addAction(Action())
                 selectedSubGoal.value = selectedSubGoal.value
             }
