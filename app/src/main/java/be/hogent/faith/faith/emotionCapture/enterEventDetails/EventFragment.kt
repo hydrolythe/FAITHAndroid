cc6c59330c8fee4af9cb1dbac8cc55b3cddbf8a6
Ozgur Akin <ozgur.akin@outlook.com>
2020-12-18 17:30:13 +0100

    Fix bug

--- app/src/main/java/be/hogent/faith/faith/emotionCapture/enterEventDetails/EventFragment.kt
+++ app/src/main/java/be/hogent/faith/faith/emotionCapture/enterEventDetails/EventFragment.kt
@@ -135,7 +135,7 @@ class EventFragment : Fragment() {
         userViewModel.user.observe(this, Observer { user ->
             Glide.with(requireContext())
                 .load(avatarProvider.getAvatarDrawableZitten(user.avatarName))
-                .diskCacheStrategy(DiskCacheStrategy.ALL).into(img_event_details_avatar_zittend)
+                .diskCacheStrategy(DiskCacheStrategy.NONE).into(img_event_details_avatar_zittend)
             Glide.with(requireContext())
                 .load(avatarProvider.getAvatarDrawableGezicht(user.avatarName))
                 .diskCacheStrategy(DiskCacheStrategy.ALL).into(img_event_details_avatar_inkleuren)
